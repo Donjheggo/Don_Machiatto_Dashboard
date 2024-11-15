b59056cd8a97f2522ed22dac5d75a3bae7ca08bf
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-11-15 19:52:10 +0800

    fix: order table error

--- components/dashboard/orders-table.tsx
+++ components/dashboard/orders-table.tsx
@@ -1,4 +1,11 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 import {
   Table,
   TableBody,
@@ -8,11 +15,10 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { GetOrders } from "@/lib/actions/orders";
-import { Badge } from "../ui/badge";
-import { MoveUpRight } from "lucide-react";
-import { Button } from "../ui/button";
+import ItemsDialog from "../orders/items-dialog";
 import Link from "next/link";
-import Image from "next/image";
+import { Button } from "../ui/button";
+import { MoveUpRight } from "lucide-react";
 
 export default async function OrdersTable({
   searchQuery,
@@ -44,44 +50,23 @@ export default async function OrdersTable({
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead className="hidden w-[100px] sm:table-cell">
-                <span className="sr-only">Image</span>
-              </TableHead>
+              <TableHead className="table-cell">Items</TableHead>
               <TableHead className="table-cell">Order no.</TableHead>
-              <TableHead className="table-cell">Product</TableHead>
-              <TableHead className="table-cell">Size</TableHead>
               <TableHead className="table-cell">Name</TableHead>
-              <TableHead className="table-cell">Quantity</TableHead>
               <TableHead className="table-cell">Total Price</TableHead>
+              <TableHead className="table-cell">Created At</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {orders?.map((item, index) => (
               <TableRow key={index}>
-                <TableCell className="hidden sm:table-cell">
-                  <Image
-                    alt="Product image"
-                    className="aspect-square rounded-md object-cover"
-                    height="64"
-                    src={item.product_id.image}
-                    width="64"
-                  />
+                <TableCell className="">
+                  <ItemsDialog order_id={item.id} />
                 </TableCell>
+                <TableCell>{item.order_number}</TableCell>
+                <TableCell>{item.name}</TableCell>
                 <TableCell className="font-normal">
-                  {item.order_number}
-                </TableCell>
-                <TableCell className="font-normal">
-                  {item.product_id.name}
-                </TableCell>
-                <TableCell className="font-normal">{item.name}</TableCell>
-                <TableCell className="font-normal">
-                  <Badge variant="default">{item.size}</Badge>
-                </TableCell>
-                <TableCell className="font-normal">
-                  {Number(item.quantity)}
-                </TableCell>
-                <TableCell className="font-normal">
-                  ₱{Number(item.product_id.price) * Number(item.quantity)}
+                  ₱{Number(item.total_price)}
                 </TableCell>
                 <TableCell className="font-normal">
                   {new Date(item.created_at).toLocaleDateString()}
